be49fe3ae0be9c2e06b65e6089e1f5d7e2fdb19b
Nick Redmond <nicholas.john.redmond@gmail.com>
2018-05-05 18:34:02 -0600

    fixed some styling issues with the carousel

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -11,9 +11,29 @@ export class NgbdCarousel implements OnInit {
   constructor(private _http: HttpClient) {}
 
   ngOnInit() {
+    this.setCarouselMaxSize();
   }
 
   getImageSource(topStory: TopStory): string {
     return environment.apiUrl + 'whyquit/images/' + topStory.imageFilename;
   }
+
+  setImageMaxSize(imageElement: HTMLImageElement) {
+    let maxCarouselHeight = this.getMaxCarouselHeight();
+    imageElement.style.maxHeight = maxCarouselHeight + "px";
+
+    let scalingFactor = maxCarouselHeight / imageElement.naturalHeight;
+    imageElement.style.maxWidth = (imageElement.naturalWidth * scalingFactor) + "px";
+  }
+
+  private setCarouselMaxSize() {
+    let maxCarouselHeight = this.getMaxCarouselHeight();
+    document.getElementById("topStoriesCarouselElement").style.maxHeight = maxCarouselHeight + "px";
+  }
+
+  private getMaxCarouselHeight(): number {
+    let windowHeight = window.innerHeight;
+    var BUFFER_HEIGHT = 75;
+    return windowHeight - BUFFER_HEIGHT;
+  };
 }

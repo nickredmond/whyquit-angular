a10cd3e3567881ea86c80b83064d641a23fb2b36
Nick Redmond <nicholas.john.redmond@gmail.com>
2018-05-12 08:51:19 -0600

    fuck it 1

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 app.use(express.static(__dirname + "/dist"));
 
-app.get("/*", function(request, response) {
+app.get("*", function(request, response) {
     response.sendFile(path.join(__dirname + "/dist/whyquit-angular/index.html"));
 });
 

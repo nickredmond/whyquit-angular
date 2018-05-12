527e05cc9bcaedc4614bc6d4f05e1acb2d3f627e
Nick Redmond <nicholas.john.redmond@gmail.com>
2018-05-12 08:22:42 -0600

    added routing support to server file

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.use(express.static(__dirname + "/dist"));
+
+app.get("/*", function(request, response) {
+    response.sendFile(path.join(__dirname + "/dist/index/whyquit-angular/index.html"));
+});
 
 app.listen(process.env.PORT || 8080);
